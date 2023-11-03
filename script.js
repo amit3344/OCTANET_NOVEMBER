9eebc82adeaaa6305312efa5746fa0c82b4ad32b
AMIT <amitbehera3344@gmail.com>
2023-11-03 10:27:11 +0530

    add java script

---
+++ script.js
@@ -0,0 +1,9 @@
+let navlinks= document.getElementById("navlinks");
+        function showmenu(){
+            navlinks.style.right ="0";
+
+        }
+        function hidemenu(){
+            navlinks.style.right ="-200px";
+            
+        }
